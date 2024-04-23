b015b4dcec484e1843bbaa90a23e7c0106e9f03b
266490 <cciric44@gmail.com>
2024-04-22 20:54:21 -0400

    added a /testing route

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const testRoute = require('./controllers/testing')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/testing', testRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
