a22f41c4e8704709e7bc2b95f1262f3a9b08de25
266490 <cciric44@gmail.com>
2024-05-05 14:06:25 -0400

    new test route

--- controllers/testing.js
+++ controllers/testing.js
@@ -6,4 +6,8 @@ router.get('/test', (req, res) => {
   res.send('this is a test')
 });
 
+router.get('/anothertest', (req, res) => {
+  res.send('this is another different test');
+});
+
 module.exports = router;
